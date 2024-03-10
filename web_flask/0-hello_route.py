b69636126454e6e957df9a0e4a33da659d1721bb
EJMarkle <emarkle99@gmail.com>
2024-03-10 15:59:54 -0500

    pep8

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Starts the HBNB Flask application"""
-
-
 from flask import Flask
+
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
+
 def hello_hbnb():
     return 'Hello HBNB!'
 
